856cc1aaa08991395457bc66d178297736836c02
KarolWes <karol.wesolowski01@gmail.com>
2023-10-12 11:23:21 +0200

    Zajęcia 1

---
+++ Minutnik1/Minutnik.cs
@@ -0,0 +1,43 @@
+﻿namespace wizualne;
+
+public class Minutnik
+{
+
+    private DateTime _endTime;
+    private DateTime _today;
+    private String _last, _diff;
+
+    public Minutnik(DateTime endTime)
+    {
+        _today = DateTime.Now;
+        _last = "";
+        _diff = "";
+        this._endTime = endTime;
+    }
+
+    public Minutnik(int hour, int minute, int sek)
+    {
+        _last = "";
+        _diff = "";
+        _today = DateTime.Now;
+        this._endTime = new DateTime(_today.Year, _today.Month, _today.Day, hour, minute, sek);
+        
+    }
+    public void show()
+    {
+        while (_today < _endTime)
+        {
+            _today = DateTime.Now;
+            _diff = (_endTime - _today).ToString().Substring(0,8);
+            if (_last != _diff)
+            {
+                // Console.Clear();
+                Console.SetCursorPosition(Console.WindowWidth-8, 0);
+                Console.WriteLine(_diff);
+                _last = _diff;
+            }
+        }
+        Console.WriteLine("Finished");
+    }
+}
+    
---
+++ minutnik/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace wizualne
+{
+    
+    class Demo
+    {
+
+        public static int Sum(params int[] args)
+        {
+            int res = 0;
+            foreach (var num in args)
+            {
+                res += num;
+            }
+
+            return res;
+        }
+
+        public static int SumAvg(out float avg, params int[] args)
+        {
+            int res = 0;
+            foreach (var num in args)
+            {
+                res += num;
+            }
+
+            avg = res / args.Length;
+            return res;
+        }
+        
+        static int Main(String[] args)
+        {
+            Minutnik m = new Minutnik(10, 40, 30);
+            m.show();
+            int[] tab = new int[(int)1e6];
+            var start = DateTime.Now;
+            for (int i = 0; i < (int)1e6; i++)
+            {
+                tab[i] = 1;
+            }
+            var res = tab.Sum();
+            var diff = DateTime.Now - start;
+            Console.WriteLine(res);
+            Console.WriteLine("Czas: {0}", diff);
+
+            object[] tab2 = new object[(int)1e6];
+            start = DateTime.Now;
+            for (int i = 0; i < (int)1e6; i++)
+            {
+                tab2[i] = 1;
+            }
+
+            res = tab2.Sum(o => (int)o);
+            diff = DateTime.Now - start;
+            Console.WriteLine(res);
+            Console.WriteLine("Czas: {0}", diff);
+            
+            Console.WriteLine(Sum(1,2,3));
+            float avg = 0.0f;
+            Console.WriteLine(SumAvg(out avg,2,3,4));
+            Console.WriteLine(avg);
+            
+            return 0;
+        }
+    }
+}
+
